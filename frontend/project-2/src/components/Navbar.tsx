9da16de98ac7cf470bda23779ece94716a5a860d
Daniel Olguin <dani.olg@hotmail.com>
2024-05-18 00:09:25 -0400

    feat: customize navbar

--- frontend/project-2/src/components/Navbar.tsx
+++ frontend/project-2/src/components/Navbar.tsx
@@ -1,26 +1,22 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import {Navbar, Container, Nav, Image} from 'react-bootstrap';
 
 function AppNavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className="font-bold">
+          <Image className="w-12 mr-2 rounded-circle" src="/images/logo.jpg" />
+          <span className="text-company-primary">Energy</span>
+          <span> & </span>
+          <span className="text-company-secondary">Water SpA</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Historia</Nav.Link>
+            <Nav.Link href="#team">Equipo</Nav.Link>
+            <Nav.Link href="#faq">Preguntas frecuentes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
