451880c6ea51f3a85633a98264c3e0f89db5f8fc
Daniel Olguin <dani.olg@hotmail.com>
2024-05-18 00:51:37 -0400

    feat: switch bewtween pages as tabs

--- frontend/project-2/src/App.tsx
+++ frontend/project-2/src/App.tsx
@@ -1,9 +1,29 @@
+import Tab from 'react-bootstrap/Tab';
 import AppNavbar from "./components/Navbar.tsx";
+import {useState} from "react";
 
 function App() {
+  const [activeKey, setActiveKey] = useState('home');
+
   return (
     <main>
-      <AppNavbar/>
+      <Tab.Container activeKey={activeKey} defaultActiveKey="home">
+        <AppNavbar activeKey={activeKey} setActiveKey={setActiveKey}/>
+        <Tab.Content>
+          <Tab.Pane eventKey="home">
+            <h1>Home</h1>
+          </Tab.Pane>
+          <Tab.Pane eventKey="history">
+            <h1>History</h1>
+          </Tab.Pane>
+          <Tab.Pane eventKey="team">
+            <h1>Team</h1>
+          </Tab.Pane>
+          <Tab.Pane eventKey="faq">
+            <h1>FAQ</h1>
+          </Tab.Pane>
+        </Tab.Content>
+      </Tab.Container>
     </main>)
 }
 
--- frontend/project-2/src/components/Navbar.tsx
+++ frontend/project-2/src/components/Navbar.tsx
@@ -1,22 +1,34 @@
 import {Navbar, Container, Nav, Image} from 'react-bootstrap';
 
-function AppNavbar() {
+interface AppNavbarProps {
+  activeKey?: string,
+  setActiveKey?: (value: (((prevState: string) => string) | string)) => void
+}
+
+function AppNavbar({activeKey, setActiveKey}: AppNavbarProps) {
+
+  function handleChangeTab(e) {
+    if (setActiveKey) {
+      setActiveKey(e.target.accessKey || 'home');
+    }
+  }
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home" className="font-bold">
-          <Image className="w-12 mr-2 rounded-circle" src="/images/logo.jpg" />
+        <Navbar.Brand onClick={handleChangeTab} className="font-bold cursor-pointer">
+          <Image className="w-12 mr-2 rounded-circle" src="/images/logo.jpg"/>
           <span className="text-company-primary">Energy</span>
           <span> & </span>
           <span className="text-company-secondary">Water SpA</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#link">Historia</Nav.Link>
-            <Nav.Link href="#team">Equipo</Nav.Link>
-            <Nav.Link href="#faq">Preguntas frecuentes</Nav.Link>
+            <Nav.Link onClick={handleChangeTab} active={activeKey === 'home'} accessKey='home'>Inicio</Nav.Link>
+            <Nav.Link onClick={handleChangeTab} active={activeKey === 'history'} accessKey='history'>Historia</Nav.Link>
+            <Nav.Link onClick={handleChangeTab} active={activeKey === 'team'} accessKey='team'>Equipo</Nav.Link>
+            <Nav.Link onClick={handleChangeTab} active={activeKey === 'faq'} accessKey='faq'>Preguntas Frecuentes</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
